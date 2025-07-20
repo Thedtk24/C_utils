ea46eea8c7ec9969f42c4130531c8fb7e2cd922a
Thedtk24 <thdarthur@gmail.com>
2025-07-20 23:34:02 +0200

    fichier d'entête pour la bibliothèque math python en C

---
+++ include/math_utils.h
@@ -0,0 +1,11 @@
+#ifndef MATH_UTILS_C
+#define MATH_UTILS_C
+
+int math_factorial(int n);
+int math_gcd(int a, int b);
+int math_lcm(int a, int b);
+double math_mean(double* arr, int size);
+double math_median(double* arr, int size);
+double math_std(double* arr, int size);
+
+#endif
