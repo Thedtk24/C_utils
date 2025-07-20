3effdfc153d5fd6054b229b4a6f2a214efc9c21e
Thedtk24 <thdarthur@gmail.com>
2025-07-20 22:18:02 +0200

    fichier d'entête pour la bibliothèque string python en C

--- include/string_utils.h
+++ include/string_utils.h
@@ -1,8 +1,13 @@
 #ifndef STRING_UTILS_H
 #define STRING_UTILS_H
 
-char* str_strip(const char* s);
-char* str_lower(const char* s); 
-char* str_upper(const char* s);
+char* str_strip(char* s);
+char* str_lower(char* s); 
+char* str_upper(char* s);
+char* str_replace(char* s, char* old, char* New);
+int str_startswith(char* s, char* prefix);
+int str_endswith(char* s, char* suffix);
+char** str_split(char* s, char* sep, int* out_count); 
+char* str_join(char* sep, char** parts, int count);
 
 #endif
